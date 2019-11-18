b3a4ef56a3576c523aa9b6d1b1e843d09219c903
Ellie <elliemmanifold@gmail.com>
2019-11-18 14:28:53 +0000

    Create balance method in Oystercard class

---
+++ project-notes.md
@@ -0,0 +1,4 @@
+### Challenge 3
+
+Type of error: NameError
+File path where error happened: ./spec/Oystercard_spec.rb. (line 1)
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'Oystercard'
 
+describe Oystercard do
+   describe '#balance' do
+     it 'balance on card should be 0' do
+       expect(subject.balance).to eq 0
+     end
+   end
 end
