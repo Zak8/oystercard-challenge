6ae2b4fd80a97686ae0a964ba8687ce123b1131f
Zak8 <zak00rashid@gmail.com>
2019-11-19 16:18:41 +0000

    merge conflict

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,12 +2,14 @@ class Oystercard
 
   BALANCE_LIMIT = 90
   MINIMUM_AMOUNT = 1
-  attr_accessor :balance, :entry_station
+  attr_accessor :balance, :entry_station, :exit_station
 
   def initialize
     @balance = 0
     @minimum_amount = MINIMUM_AMOUNT
     @entry_station = nil
+    @array = []
+    @exit_station = nil
   end
 
   def top_up(amount)
@@ -25,15 +27,21 @@ class Oystercard
     @entry_station = entry_station.name
   end
 
-  def touch_out
+  def touch_out(exit_station)
     @in_journey = false
     deduct(MINIMUM_AMOUNT)
-    @entry_station = nil
+    @exit_station = exit_station.name
+    @array.push({entry: @entry_station, exit: @exit_station})
   end
 
   private
-  
+
   def deduct(amount)
     @balance -= amount
+  end
+
+  def Combine_St_In_Out
+    hash_of_stations = Hash.new
+    hash_of_stations[@entry_station] = @exit_station
   end
 end
